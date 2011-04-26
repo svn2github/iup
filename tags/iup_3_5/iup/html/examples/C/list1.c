3886785117cd82d5ff5d9c5e9f642463f71c27bc
(no author) <(no author)@2f08bc20-984a-4df8-b195-9b5d5d477537>
2011-04-26 20:54:03 +0000

    This commit was manufactured by cvs2svn to create tag 'iup_3_5'.
    git-svn-id: http://svn.code.sf.net/p/iup/iup@1117 2f08bc20-984a-4df8-b195-9b5d5d477537

---
+++ tags/iup_3_5/iup/html/examples/C/list1.c
@@ -0,0 +1,58 @@
+/* IupList: Example in C 
+   Creates a dialog with three frames, each one containing a list.
+   The first is a simple list, the second one is a multiple list and the last one is a drop-down list.
+   The second list has a callback associated.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iup.h>
+
+int list_multiple_cb (Ihandle *self, char *t, int i, int v)
+{
+  printf ("Item %d - %s - %s\n", i, t, v == 0 ? "deselected" : "selected" );
+  return IUP_DEFAULT;
+}
+
+int main(int argc, char **argv) 
+{
+  Ihandle *dlg;
+  Ihandle *list, *list_multiple, *list_dropdown;
+  Ihandle *frm_medal, *frm_sport, *frm_prize;
+
+  IupOpen(&argc, &argv);
+
+  list = IupList ("list_act");
+  IupSetAttributes (list, "1=Gold, 2=Silver, 3=Bronze, 4=None,"
+                          "VALUE=4, SIZE=EIGHTHxEIGHTH");
+
+  frm_medal = IupFrame (list);
+  IupSetAttribute (frm_medal, "TITLE", "Best medal");
+  list_multiple = IupList(NULL);
+  
+  IupSetAttributes (list_multiple, "1=\"100m dash\", 2=\"Long jump\", 3=\"Javelin throw\", 4=\"110m hurdlers\", 5=\"Hammer throw\",6=\"High jump\","
+                                   "MULTIPLE=YES, VALUE=\"+--+--\", SIZE=EIGHTHxEIGHTH");
+
+  IupSetCallback(list_multiple, "ACTION", (Icallback)list_multiple_cb);
+  
+  frm_sport = IupFrame (list_multiple);
+  
+  IupSetAttribute (frm_sport, "TITLE", "Competed in");
+
+  list_dropdown = IupList (NULL);
+  
+  IupSetAttributes (list_dropdown, "1=\"Less than US$ 1000\", 2=\"US$ 2000\", 3=\"US$ 5000\", 4=\"US$ 10000\", 5=\"US$ 20000\", 6=\"US$ 50000\", 7=\"More than US$ 100000\","
+                                   "DROPDOWN=YES, VISIBLE_ITEMS=5");
+  
+  frm_prize = IupFrame (list_dropdown);
+  IupSetAttribute (frm_prize, "TITLE", "Prizes won");
+
+  dlg = IupDialog (IupHbox (frm_medal, frm_sport, frm_prize, NULL));
+  IupSetAttribute (dlg, "TITLE", "IupList Example");
+  IupShowXY (dlg, IUP_CENTER, IUP_CENTER);
+
+  IupMainLoop ();
+  IupClose ();
+  return EXIT_SUCCESS;
+
+}
